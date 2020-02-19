c5229d7e96699f5771aeb3874d2238925a32359b
Chathura Sil <johnnzsilva@gmail.com>
2020-02-19 13:51:44 +1300

    Changed the return view name

--- YoobeeAPI/Controllers/HomeController.cs
+++ YoobeeAPI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace YoobeeAPI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public ActionResult About()
